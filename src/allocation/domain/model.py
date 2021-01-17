6b53870c603d8cab5df8588bc250e52205915e06
Gabriel <gabriel59kg@gmail.com>
2021-01-17 18:28:44 -0500

    pydantic dataclass

--- src/allocation/domain/model.py
+++ src/allocation/domain/model.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from dataclasses import dataclass
+from pydantic.dataclasses import dataclass
 from datetime import date
 from typing import Optional, List, Set
 from . import commands, events
